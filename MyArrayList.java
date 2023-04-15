27042609fd56c29902b911bd2c10b452d2e22327
Dek <karen.ananyan2017@mail.ru>
2023-04-16 01:54:26 +0600

    add: increaseCapacity method

--- MyArrayList.java
+++ MyArrayList.java
@@ -9,4 +9,15 @@ public class MyArrayList implements MyList{
     public MyArrayList(int initialCapacity) {
         hiddenArr = new Object[initialCapacity];
     }
+
+    private void increaseCapacity() {
+        int newSize = (int)(hiddenArr.length * 1.5);
+        Object[] newArr = new Object[newSize];
+
+        for (int i = 0; i < hiddenArr.length; i++) {
+            newArr[i] = hiddenArr[i];
+        }
+
+        hiddenArr = newArr;
+    }
 }
