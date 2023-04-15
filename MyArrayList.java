55ff1288f49e24d4b96dd7e74e024e043ba1c97b
Dek <karen.ananyan2017@mail.ru>
2023-04-16 02:13:43 +0600

    add: size and contains methods

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,4 +1,4 @@
-public class MyArrayList implements MyList{
+public class MyArrayList<T> implements MyList<T>{
     private Object[] hiddenArr;
     private int length;
 
@@ -19,5 +19,63 @@ public class MyArrayList implements MyList{
         }
 
         hiddenArr = newArr;
+    }
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i<length; i++){
+            if (o == hiddenArr[i]) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
     }
 }
